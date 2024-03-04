e4028a76d0cb5df5c489707b2e52b986b08d0117
Sam McLeod <sammcj@users.noreply.github.com>
2024-03-05 10:20:32 +1100

    minor updates

--- components/esp_box_volume/__init__.py
+++ components/esp_box_volume/__init__.py
@@ -1,45 +1,40 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
 from esphome.const import CONF_ID
+from esphome.codegen import void
 
 esp_box_volume_ns = cg.esphome_ns.namespace('esp_box_volume')
-
 ESPBoxVolume = esp_box_volume_ns.class_('ESPBoxVolume', cg.Component)
 
-output_volume = esp_box_volume_ns.class_('FloatOutput', output.FloatOutput)
+# Define the service within the component
+set_volume = ESPBoxVolume.add_method('set_volume', void, [(float, 'volume')])
 
-# add the speaker output
-output.register_output(output_volume, 'FloatOutput')
-
-# add the service to the namespace
-set_volume = esp_box_volume_ns.service('set_volume')
-set_volume_level = set_volume.template('set_level', cg.uint8)
-
-# Add the service to the class
-ESPBoxVolume.add(set_volume_level)
-
-# add the output to the component
-ESPBoxVolume.add(output_volume)
-
-# Add the service to the component
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ESPBoxVolume),
+    # Define a schema for the service call, if needed
+    cv.Optional("default_volume"): cv.percentage,
+}).extend(cv.COMPONENT_SCHEMA)
+
+# Declare the ID for the component
+CONF_ESP_BOX_VOLUME_ID = 'esp_box_volume_id'
+
+# Service definition
+def set_volume_args(var, config):
+    arg = config['volume']
+    template_ = yield cg.templatable(arg, var, cg.float_)
+    cg.add(var.set_volume(template_))
+
+# Register the service
+@ESPBoxVolume.register_service('set_volume', 'float')
+def set_volume(var, config):
+    yield set_volume_args(var, config)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ESPBoxVolume),
+    cv.GenerateID(CONF_ESP_BOX_VOLUME_ID): cv.use_id(ESPBoxVolume),
+    # Additional configurations here
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-
-    var.set_volume_level = cg.new_Pvariable(set_volume_level, var)
-    yield cg.register_service(set_volume_level, var)
-
-
-
-    # Add the service to the component
-    cg.add(set_volume_level.set_component(var))
-
-    # Add the service to the class
-    cg.add(set_volume_level.set_callback(var.set_volume_level_callback))
-
-    yield cg.register_service(set_volume_level, var)
