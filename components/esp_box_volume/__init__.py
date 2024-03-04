ba9c887d00a80a5ff3c813f43d180a2a709f0809
Sam McLeod <sammcj@users.noreply.github.com>
2024-03-05 06:48:51 +1100

    minor updates

--- components/esp_box_volume/__init__.py
+++ components/esp_box_volume/__init__.py
@@ -1,22 +1,12 @@
 import esphome.codegen as cg
-from esphome import core
-from esphome.core import coroutine_with_priority, CORE
-from esphome import automation
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-
-DEPENDENCIES = ['esp32']
+from esphome.core import CORE
 
 esp_box_volume_ns = cg.esphome_ns.namespace('esp_box_volume')
 EspBoxVolume = esp_box_volume_ns.class_('EspBoxVolume', cg.Component)
 
-CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EspBoxVolume),
-    # Define your component's configuration options here
-})
-
-@coroutine_with_priority(100.0)
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    # Setup actions or configurations specific to this component
+    cv.Optional('volume', default=50): cv.All(cv.int_range(min=0, max=100), cv.Coerce(int)),
+}).extend(cv.COMPONENT_SCHEMA)
