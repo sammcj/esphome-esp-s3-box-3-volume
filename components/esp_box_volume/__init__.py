48b60239a42d3a40de21b49053cfcd247e474dcb
Sam McLeod <sammcj@users.noreply.github.com>
2024-03-05 06:47:42 +1100

    minor updates

--- components/esp_box_volume/__init__.py
+++ components/esp_box_volume/__init__.py
@@ -1,19 +1,22 @@
 import esphome.codegen as cg
+from esphome import core
+from esphome.core import coroutine_with_priority, CORE
 from esphome import automation
-from esphome.components import esp32
-from esphome.core import CORE, coroutine
+import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# Import your C++ classes
+DEPENDENCIES = ['esp32']
+
 esp_box_volume_ns = cg.esphome_ns.namespace('esp_box_volume')
 EspBoxVolume = esp_box_volume_ns.class_('EspBoxVolume', cg.Component)
 
-# Define configuration variables
-CONFIG_SCHEMA = esp32.ESP_PLATFORM_SCHEMA.extend({
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(EspBoxVolume),
+    # Define your component's configuration options here
+})
 
-}).extend(cv.COMPONENT_SCHEMA)
-
+@coroutine_with_priority(100.0)
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
+    # Setup actions or configurations specific to this component
