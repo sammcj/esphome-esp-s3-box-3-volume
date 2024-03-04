c747918de5871512af05ff810ad65f8c69adaf3a
Sam McLeod <sammcj@users.noreply.github.com>
2024-03-05 09:10:36 +1100

    minor updates

--- components/esp_box_volume/__init__.py
+++ components/esp_box_volume/__init__.py
@@ -4,13 +4,15 @@ from esphome.components import output
 from esphome.const import CONF_ID
 
 esp_box_volume_ns = cg.esphome_ns.namespace('esp_box_volume')
-ESPBoxVolume = esp_box_volume_ns.class_('ESPBoxVolume', cg.Component, output.FloatOutput)
+ESPBoxVolume = esp_box_volume_ns.class_('ESPBoxVolume', output.FloatOutput, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(ESPBoxVolume),
-}).extend(cv.COMPONENT_SCHEMA).extend(output.FLOAT_OUTPUT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield output.register_output(var, config)
+
+    cg.add(var.set_name("esp_box_volume"))
