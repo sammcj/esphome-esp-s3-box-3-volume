3557bae6cd4944c48c6b234661144821ccad3bcd
Sam McLeod <sammcj@users.noreply.github.com>
2024-03-05 09:57:12 +1100

    minor updates

--- components/esp_box_volume/__init__.py
+++ components/esp_box_volume/__init__.py
@@ -12,7 +12,7 @@ output.FloatOutput = esp_box_volume_ns.class_('FloatOutput', output.FloatOutput)
 
 # add the service to the namespace
 set_volume = esp_box_volume_ns.service('set_volume')
-set_volume_level = set_volume.template('set_volume_level', cg.uint8)
+set_volume_level = set_volume.template('set_level', cg.uint8)
 
 # Add the service to the class
 ESPBoxVolume.add(set_volume_level)
